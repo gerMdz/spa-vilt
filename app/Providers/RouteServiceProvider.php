5d19cf030a7787069c55025d774a9ac231069bfb
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2024-03-12 08:40:00 -0300

    Cap 24 iniciando dashboard de admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('dashboard')
+                ->middleware(['web', 'auth'])
+                ->namespace($this->namespace)
+                ->group(base_path('routes/dashboard.php'));
         });
     }
 
---
+++ routes/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\SkillController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::resource('habilidades', SkillController::class)
+->except(['create', 'show', 'edit']);
+Route::resource('proyectos', ProjectController::class)
+->except(['create', 'show', 'edit']);
